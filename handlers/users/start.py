3082589f085dd37b2a2b9ec706c89ce5664e0992
klen_dev <klenmrx@gmail.com>
2023-02-05 01:06:11 +0500

    last commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,3 +7,7 @@ from loader import dp
 @dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
+
+@dp.message_handler(IsPrivate(), commands='admins')
+async def bot_admins(message: types.Message):
+    await message.answer(f"Adminlar: @klen_uz va @H_Grenger")    
