187c4216e6519d9fb148c755bebb7afe087a3d38
klen_dev <klenmrx@gmail.com>
2023-02-08 12:16:37 +0500

    last commit

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,8 +6,12 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
+    text = ("<b>Buyruqlar: ",
             "/start - Botni ishga tushirish",
-            "/help - Yordam")
+            "/help - Yordam",
+            "!mute - Mute berish",
+            "!unmute - Mutedan chiqarish",
+            "!bayroq - Bayroq o'rnatish",
+            "So'kinganlarga o'zim 2 soat mute beraman</b>")
     
     await message.answer("\n".join(text))
