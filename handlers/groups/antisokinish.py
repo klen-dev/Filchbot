237170e6788a7cce1ab5e60a5d6e14e1996e4448
klen_dev <klenmrx@gmail.com>
2023-02-08 12:13:49 +0500

    last commit

--- handlers/groups/antisokinish.py
+++ handlers/groups/antisokinish.py
@@ -11,8 +11,8 @@ from aiogram.utils.exceptions import BadRequest
 ADMIN=5828291838
 sokinishlar = ['jalab','zaybal','chuchoq','tashshoq','joulup', 'jalla', 'seks', 'foxisha', 'dalbayob', 'sikaman', 'qotoq', 'tashoq', 'tashaq', 'dalbayob',
                'qotoq','gandon','гандон', 'qutoq','жалаб','жоулуп','жалла',   'секс', 'фохиша', 'далбаёб', 'сикаман', 'қотоқ', 'ташоқ', 'ташақ', 'далбаёб',
-               'қотоқ', 'қутоқ','зайбал','заебал', 'yban','хуйет','🏳️‍🌈']
-sokinishlar2 = ['nx','gey','xuyyet', 'xuet','kt','kot', 'kut','dlb','om','am', 'jlb', 'gone done', 'joul up', ' one nine sky','dnx', 'pnx','sky','кт','кот', 'кут','длб','ом','ам', 'жлб', 'гоне доне', 'жоул уп', ' оне нине скй','днх', 'пнх','скй']
+               'қотоқ', 'қутоқ','зайбал','заебал', 'yban','хуйет','🏳️‍🌈', '@dnx', '@pnx', '@dnx', '@pnx','yiban']
+sokinishlar2 = ['kutanak', 'nx','gey','xuyyet', 'xuet','kt','kot', 'kut','dlb','om','am', 'jlb', 'gone done', 'joul up', ' one nine sky','dnx', 'pnx','sky','кт','кот', 'кут','длб','ом','ам', 'жлб', 'гоне доне', 'жоул уп', ' оне нине скй','днх', 'пнх','скй']
 
 for i in sokinishlar:
     @dp.message_handler(IsGroup(), Text(equals=sokinishlar2, ignore_case=True))
@@ -25,14 +25,14 @@ for i in sokinishlar:
         try:
             await message.chat.restrict(user_id=message.from_user.id, can_send_messages=False, until_date=until_date)
             sokinmang_message = await message.answer(f"❗️<b>{message.from_user.mention} vaqtincha mute holatiga tushdingiz!\n</b>"
-                                                f"<b>Sabab: <i>Qo'pol so'zlar ishlatgani uchun</i></b>")
-            await asyncio.sleep(15)
-            await sokinmang_message.delete()
+                                                f"<b>Sabab: <i>Guruhda taqiqlangan so'zlar ishlatgani uchun</i></b>")
+            # await asyncio.sleep(15)
+            # await sokinmang_message.delete()
         except aiogram.utils.exceptions.BadRequest as err:
             sokinmang_message = await message.answer(f"❗️<b>Hurmatli {message.from_user.mention} tilingizga qat'iy ehtiyot bo'ling!</b>")
-            await dp.bot.send_message(ADMIN, f"{message.from_user.mention} guruhda so'kindi! \n So'zlar: {message.text}")
-            await asyncio.sleep(10)
-            await sokinmang_message.delete()
+            await dp.bot.send_message(ADMIN, f"<b>{message.from_user.mention} guruhda so'kindi! \nSo'zlar: {message.text}</b>")
+            # await asyncio.sleep(10)
+            # await sokinmang_message.delete()
             return
 
     
