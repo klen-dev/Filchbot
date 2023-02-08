a6c9dfc15846bbd145272358ed11f7dd1fa11e98
klen_dev <klenmrx@gmail.com>
2023-02-08 12:06:16 +0500

    last commit

--- handlers/groups/antisokinish.py
+++ handlers/groups/antisokinish.py
@@ -24,16 +24,16 @@ for i in sokinishlar:
         await message.delete()
         try:
             await message.chat.restrict(user_id=message.from_user.id, can_send_messages=False, until_date=until_date)
-            await message.answer(f"❗️<b>{message.from_user.mention} vaqtincha mute holatiga tushdingiz!\n</b>"
+            sokinmang_message = await message.answer(f"❗️<b>{message.from_user.mention} vaqtincha mute holatiga tushdingiz!\n</b>"
                                                 f"<b>Sabab: <i>Taqiqlangan so'z ishlatgani uchun</i></b>")
             await dp.bot.send_message(ADMIN, f"{message.from_user.mention} guruhda so'kindi! \nSo'zlar: {message.text}")
-            # await asyncio.sleep()
-            # await sokinmang_message.delete()
+            await asyncio.sleep(600)
+            await sokinmang_message.delete()
         except aiogram.utils.exceptions.BadRequest as err:
-            await message.answer(f"❗️<b>Hurmatli {message.from_user.mention} tilingizga ehtiyot bo'ling!</b>")
+            sokinmang_message = await message.answer(f"❗️<b>Hurmatli {message.from_user.mention} tilingizga ehtiyot bo'ling!</b>")
             await dp.bot.send_message(ADMIN, f"❗️<b>{message.from_user.mention} guruhda so'kindi! \nSo'zlar: {message.text}</b>")
-            # await asyncio.sleep(120)
-            # await sokinmang_message.delete()
+            await asyncio.sleep(600)
+            await sokinmang_message.delete()
             return
 
     
