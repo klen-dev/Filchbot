e1936072f4ee745b4f3ae3d3024140afab822fd2
klen_dev <klenmrx@gmail.com>
2023-02-08 12:45:07 +0500

    last commit

--- handlers/groups/antisokinish.py
+++ handlers/groups/antisokinish.py
@@ -24,12 +24,12 @@ for i in sokinishlar:
         await message.delete()
         try:
             await message.chat.restrict(user_id=message.from_user.id, can_send_messages=False, until_date=until_date)
-            sokinmang_message = await message.answer(f"❗️<b>{message.from_user.mention} vaqtincha mute holatiga tushdingiz!\n</b>"
+            await message.answer(f"❗️<b>{message.from_user.mention} vaqtincha mute holatiga tushdingiz!\n</b>"
                                                 f"<b>Sabab: <i>Guruhda taqiqlangan so'zlar ishlatgani uchun</i></b>")
-            # await asyncio.sleep(15)
+            await asyncio.sleep(15)
             # await sokinmang_message.delete()
         except aiogram.utils.exceptions.BadRequest as err:
-            sokinmang_message = await message.answer(f"❗️<b>Hurmatli {message.from_user.mention} tilingizga qat'iy ehtiyot bo'ling!</b>")
+            sokinmang_message = await message.answer(f"❗️<b>Hurmatli {message.from_user.mention} tilingizga qat'iy ehtiyot bo'ling! \n</b>")
             await dp.bot.send_message(ADMIN, f"<b>{message.from_user.mention} guruhda so'kindi! \nSo'zlar: {message.text}</b>")
             # await asyncio.sleep(10)
             # await sokinmang_message.delete()
